9306fbeb0efbd0b3e9df4f8716e35b4779d5027d
Kangur <urbanek.kamil@gmail.com>
2023-08-14 10:05:46 +0200

    Add new subpage notes

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,15 +3,6 @@ PORT = "8080"
 SITEURL = f'{URL}:{PORT}'
 RELATIVE_URLS = True
 
-SITENAME = "Kamil's Scratchpad"
-SITEINFO = "Welcome to my digital notepad, a space where I share my notes and thoughts on programming, pipelines, automation, and an array of other, yet to be discovered, buzzwords."
-SITELINKS = (
-    # ('About', 'about/index.html', 'file-person'),
-    # ('About', 'resume.html', 'file-person'),
-    ('About', 'under_construction.html', 'file-person'),
-    ('Git', "https://github.com/Kanguros/", 'github'),
-)
-
 TIMEZONE = 'Europe/Warsaw'
 AUTHOR = 'Kamil Urbanek'
 DEFAULT_LANG = 'en'
@@ -28,6 +19,15 @@ PLUGINS = [
     'neighbors',
 ]
 
+SITENAME = "Kamil's Scratchpad"
+SITEINFO = "Welcome to my digital notepad, a space where I share my notes and thoughts on programming, pipelines, automation, and an array of other, yet to be discovered, buzzwords."
+SITELINKS = (
+    # ('About', 'about/index.html', 'file-person'),
+    ('About', 'resume.html', 'file-person'),
+    # ('About', 'under_construction.html', 'file-person'),
+    ('Git', "https://github.com/Kanguros/", 'github'),
+)
+
 THEME = 'theme'
 THEME_STATIC_DIR = THEME
 # THEME_DEFAULT_HEADER = "home-bg.png"
@@ -40,8 +40,14 @@ DIRECT_TEMPLATES = [
     'archives',
     'tags',
     'under_construction',
+    'notes',
     # 'resume'
 ]
+PAGINATED_DIRECT_TEMPLATES = [
+    'index',
+    'notes'
+]
+
 STATIC_PATHS = [
     'images',
     'slides',
@@ -58,7 +64,7 @@ DEFAULT_PAGINATION = 3
 PATH = 'content'
 OUTPUT_PATH = 'local_output/'
 
-ARTICLE_PATHS = ['posts']
+ARTICLE_PATHS = ['posts', 'raw_notes']
 ARTICLE_URL = 'posts/{slug}.html'
 ARTICLE_SAVE_AS = 'posts/{slug}.html'
 ARTICLE_ORDER_BY = 'reversed-date'
@@ -77,8 +83,12 @@ CATEGORIES_SAVE_AS = 'categories.html'
 
 ARCHIVES_SAVE_AS = 'archive.html'
 
+NOTES_URL = 'notes.html'
+EXCLUDED_CATEGORIES = ['notes']
+
 PAGESMENUITEMS = [
     ('Tags', TAGS_SAVE_AS, 'tags'),
+    ('Notes', NOTES_URL, 'journal-code'),
     ('Archive', ARCHIVES_SAVE_AS, 'archive'),
 ]
 
