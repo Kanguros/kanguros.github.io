42398d3fcd01fbbc57c6fab4480383f679b780ef
Kangur <urbanek.kamil@gmail.com>
2023-11-08 18:10:38 +0100

    Add links, update conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -7,7 +7,11 @@ TIMEZONE = 'Europe/Warsaw'
 AUTHOR = 'Kamil Urbanek'
 DEFAULT_LANG = 'en'
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
+# Paths and other files
+PATH = 'content'
+OUTPUT_PATH = '_output/'
 
+# Plugins
 PLUGINS = [
     'yaml_metadata',
     'similar_posts',
@@ -17,15 +21,15 @@ PLUGINS = [
     # 'minify',  In publishconf.py
 ]
 
-# readtime
+# Plugin - readtime
 LANG_SETTINGS = 120
 
-# similarposts
+# Plugin - similarposts
 SIMILAR_POSTS_ENABLED = True
 SIMILAR_POSTS_MAX_COUNT = 3
 SIMILAR_POSTS_MIN_SCORE = 0.0001
 
-# minify
+# Plugin - minify
 CSS_MIN = True
 HTML_MIN = True
 INLINE_CSS_MIN = True
@@ -35,11 +39,11 @@ SITENAME = "Kamil's Scratchpad"
 SITENAME_BRAND = "KS"
 SITEINFO = "Welcome to my digital notepad. Space where <strong>I share</strong> my <strong>notes and thoughts</strong> on <strong>programming, pipelines, automation,</strong> and an array of other, yet to be discovered, fancy stuff."
 
-THEME = 'theme'
-THEME_STATIC_DIR = THEME
+THEME_STATIC_DIR = THEME = "theme"
 COLOR_SCHEME_CSS = "darkly.css"
 SUMMARY_END_SUFFIX = ''
 TYPOGRIFY = True
+
 DIRECT_TEMPLATES = [
     'index',
     'archives',
@@ -49,7 +53,6 @@ DIRECT_TEMPLATES = [
     'homepage',
 ]
 
-TEMPLATE_PAGES = {'homepage.html': 'index.html'}
 DEFAULT_PAGINATION = 5
 PAGINATED_TEMPLATES = {
     'index': None,
@@ -68,14 +71,10 @@ EXTRA_PATH_METADATA = {
     'extra/.nojekyll': {'path': '.nojekyll'},
 }
 
-# Paths and other files
-PATH = 'content'
-OUTPUT_PATH = '_output/'
-
+TEMPLATE_PAGES = {'homepage.html': 'index.html'}
 INDEX_SAVE_AS = "all_posts.html"
-
 ARCHIVES_SAVE_AS = 'archive.html'
-
+ABOUT_URL = "about.html"
 AUTHOR_SAVE_AS = ""
 
 ARTICLE_PATHS = ['posts', 'notes']
@@ -97,28 +96,21 @@ TAGS_SAVE_AS = 'tags.html'
 CATEGORY_URL = '{slug}s.html'
 CATEGORY_SAVE_AS = '{slug}s.html'
 CATEGORIES_SAVE_AS = 'categories.html'
-CATEGORY_ARTICLES_URL = "articles.html"
-CATEGORY_NOTES_URL = "notes.html"
+
+ARTICLES_URL = "articles.html"
+ARTICLES_DESCRIPTION = "Private notes turned public: straightforward and full of unexpected insights."
+NOTES_URL = "notes.html"
+NOTES_DESCRIPTION = "Private notes make public. You read at your own risk"
 DEFAULT_CATEGORY = "Note"
 
 GIT_LINK = "https://github.com/Kanguros/"
-
-OTHER_LINKS = [
-    ('Archive', ARCHIVES_SAVE_AS, 'archive'),
-
-]
-
 NAV_LINKS = [
-    # Title, URL, Icon
-    # ('Home', 'index.html', 'house'),
-    ('Articles', CATEGORY_ARTICLES_URL, 'journal-code'),
-    ('Notes', CATEGORY_NOTES_URL, 'file-code'),
+    ('Articles', ARTICLES_URL, 'journal-code'),
+    ('Notes', NOTES_URL, 'file-code'),
     ('Tags', TAGS_SAVE_AS, 'tags'),
-    ('About', 'about.html', 'file-person'),
-]
+    ('About', ABOUT_URL, 'file-person'),
+    # ('Archive', ARCHIVES_SAVE_AS, 'archive'),
 
-ALL_LINKS = [
-    *NAV_LINKS
 ]
 
 MARKDOWN = {
