36f7c773c3c82287b27b56482552b887ea0e918d
Kangur <urbanek.kamil@gmail.com>
2025-01-08 13:58:05 +0100

    Adjust typogrify settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -50,7 +50,7 @@ THEME_STATIC_DIR = THEME = "theme"
 COLOR_SCHEME_CSS = "darkly.css"
 SUMMARY_END_SUFFIX = ''
 TYPOGRIFY = True
-TYPOGRIFY_IGNORE_TAGS = ["pre","code","svg","img","style"]
+TYPOGRIFY_IGNORE_TAGS = ["pre", "code", "svg", "img", "style"]
 
 DIRECT_TEMPLATES = [
     'index',
@@ -144,7 +144,7 @@ MARKDOWN = {
             'base_path': "content"
         },
         'pymdownx.magiclink': {},
-'pymdownx.saneheaders':{}
+        'pymdownx.saneheaders': {},
         'markdown_extension_mermaid': {},
     },
     'output_format': 'html5'
