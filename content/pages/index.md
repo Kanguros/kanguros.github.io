73d0bcd56fd73c78ed45d6640b77a9401103330d
Kangur <urbanek.kamil@gmail.com>
2025-01-19 23:09:09 +0100

    fix

--- content/pages/index.md
+++ content/pages/index.md
@@ -1,10 +1,11 @@
----
-title: Welcome to My Site
-url:
-save_as: index.html
-template: _index.html
----
+Title: index
+Url:
+Save_as: index.html
+Template: main_page
 
 # Hi! I'm Kamil
 
-Welcome
+Welcome to my digital notepad.
+Space where I share my notes and thoughts
+on programming, pipelines, automation,
+and an array of other, yet to be discovered, fancy stuff.
--- pelicanconf.py
+++ pelicanconf.py
@@ -1,26 +1,26 @@
-URL = 'http://localhost'
+URL = "http://localhost"
 PORT = "8080"
-SITEURL = f'{URL}:{PORT}'
+SITEURL = f"{URL}:{PORT}"
 RELATIVE_URLS = True
 
-TIMEZONE = 'Europe/Warsaw'
-AUTHOR = 'Kamil Urbanek'
-DEFAULT_LANG = 'en'
+TIMEZONE = "Europe/Warsaw"
+AUTHOR = "Kamil Urbanek"
+DEFAULT_LANG = "en"
 DEFAULT_DATE_FORMAT = "%Y-%m-%d"
 
 # Paths and other files
-PATH = 'content'
-OUTPUT_PATH = '_output/'
+PATH = "content"
+OUTPUT_PATH = "_output/"
 
 # Plugins
 PLUGINS = [
-    'yaml_metadata',
-    'similar_posts',
-    'readtime',
-    'neighbors',
+    "yaml_metadata",
+    "similar_posts",
+    "readtime",
+    "neighbors",
     "pelican.plugins.jinja_filters",
-    # 'sitemap',  In publishconf.py
-    # 'minify',  In publishconf.py
+    # "sitemap",  In publishconf.py
+    # "minify",  In publishconf.py
 ]
 
 # Plugin - readtime
@@ -40,73 +40,81 @@ SIMILAR_POSTS_MIN_SCORE = 0.0001
 SITENAME = "Kamil's Scratchpad"
 SITENAME_BRAND = "KS"
 SITEINFO = """
-<p>Welcome to my digital notepad.</p>
-<p>Space where <strong>I share</strong> my <strong>notes and thoughts</strong> on 
-<strong>programming, pipelines, automation,</strong> 
-and an array of other, yet to be discovered, fancy stuff.</p>
+
 """
 
 THEME_STATIC_DIR = THEME = "theme"
 COLOR_SCHEME_CSS = "darkly.css"
-SUMMARY_END_SUFFIX = ''
+SUMMARY_END_SUFFIX = ""
 # TYPOGRIFY = False
 TYPOGRIFY = True
 TYPOGRIFY_IGNORE_TAGS = ["pre", "code", "svg", "img", "style"]
 
 DIRECT_TEMPLATES = [
-    'index',
-    'archives',
-    'tags',
-    'under_construction',
-    'privates',
-    # 'posts'
+    "index",
+    "archives",
+    "tags",
+    "under_construction",
+    "private",
+    # "posts"
 ]
 
 DEFAULT_PAGINATION = 5
 PAGINATED_TEMPLATES = {
-    'index': None,
-    'tag': None,
+    "index": None,
+    "tag": None,
 }
-
-TEMPLATE_PAGES = {
-    'privates.html': 'private/index.html'
-}
+PAGINATION_PATTERNS = (
+    (1, "{url}", "{save_as}"),
+    (2, "{base_name}/{number}/", "{base_name}/{number}/index.html"),
+)
 
 STATIC_PATHS = [
-    'images',
-    'code',
-    'extra/.nojekyll',
-    # 'slides',
+    "images",
+    "code",
+    "extra/.nojekyll",
+    # "slides",
 
 ]
 EXTRA_PATH_METADATA = {
-    'extra/.nojekyll': {'path': '.nojekyll'},
+    "extra/.nojekyll": {"path": ".nojekyll"},
 }
 
-INDEX_SAVE_AS = 'post/index.html'
-ARCHIVES_SAVE_AS = 'archive.html'
-ABOUT_URL = "about.html"
+INDEX_SAVE_AS = POSTS_SAVE_AS = "posts/index.html"
+POSTS_URL = "posts/"
+ARCHIVES_URL = "archive/"
+ARCHIVES_SAVE_AS = "archive/index.html"
+
+ABOUT_URL = "about/"
+ABOUT_SAVE_AS = "about/index.html"
 
 AUTHOR_SAVE_AS = ""
 CATEGORY_SAVE_AS = ""
 CATEGORIES_SAVE_AS = ""
 
-ARTICLE_PATHS = ['private', 'posts']
-ARTICLE_URL = 'post/{slug}.html'
-ARTICLE_SAVE_AS = 'post/{slug}.html'
-ARTICLE_ORDER_BY = 'reversed-date'
+ARTICLE_PATHS = ["private", "posts"]
+ARTICLE_URL = "post/{slug}/"
+ARTICLE_SAVE_AS = "post/{slug}/index.html"
+ARTICLE_ORDER_BY = "reversed-date"
 
-DRAFTS_URL = "private/index.html"
-DRAFT_URL = 'private/{slug}.html'
-DRAFT_SAVE_AS = 'private/{slug}.html'
+TEMPLATE_PAGES = {
+    "private.html": "private/index.html"
+}
+DRAFTS_URL = "private/"
+DRAFTS_SAVE_AS = "private/index.html"
+DRAFT_URL = "private/{slug}/"
+DRAFT_SAVE_AS = "private/{slug}/index.html"
 
-PAGE_PATHS = ['pages']
-PAGE_URL = '{slug}.html'
-PAGE_SAVE_AS = '{slug}.html'
+PAGE_PATHS = ["pages"]
+PAGE_URL = "{slug}/"
+PAGE_SAVE_AS = "{slug}/index.html"
 
-TAG_URL = 'tag/{slug}.html'
-TAG_SAVE_AS = 'tag/{slug}.html'
-TAGS_SAVE_AS = 'tag/index.html'
+TAG_URL = "tag/{slug}/"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+TAGS_URL = "tags/"
+TAGS_SAVE_AS = "tags/index.html"
+
 TAGS_DESCRIPTION = {
     "python": "Knowledge of Python",
     "cheatsheet": "Shortcut of tips and tricks."
@@ -116,42 +124,42 @@ DEFAULT_CATEGORY = "post"
 
 GIT_LINK = "https://github.com/Kanguros/"
 NAV_LINKS = [
-    ('Tags', TAGS_SAVE_AS, 'tags'),
-    ('About', ABOUT_URL, 'file-person'),
-    # ('Git', GIT_LINK, 'github'),
+    ("Posts", POSTS_URL, "post"),
+    ("Tags", TAGS_URL, "tags"),
+    ("About", ABOUT_URL, "file-person"),
+    # ("Git", GIT_LINK, "github"),
 ]
 
 FOOTER_LINKS = [
-    ('Index', "", 'home'),
+    ("Index", "", "home"),
     *NAV_LINKS,
-
 ]
 
 MARKDOWN = {
-    'extension_configs': {
-        'markdown.extensions.extra': {},
-        # 'markdown.extensions.attr_list': {},
-        'markdown.extensions.admonition': {},
-        'markdown.extensions.codehilite': {
-            'css_class': 'highlight'
+    "extension_configs": {
+        "markdown.extensions.extra": {},
+        # "markdown.extensions.attr_list": {},
+        "markdown.extensions.admonition": {},
+        "markdown.extensions.codehilite": {
+            "css_class": "highlight"
         },
-        'markdown.extensions.toc': {
-            'permalink': 'false',
-            'anchorlink': 'true',
-            'toc_depth': 3,
-            'title': 'Content'
+        "markdown.extensions.toc": {
+            "permalink": "false",
+            "anchorlink": "true",
+            "toc_depth": 3,
+            "title": "Content"
         },
-        'pymdownx.progressbar': {},
-        'pymdownx.blocks.tab': {},
-        'pymdownx.snippets': {
-            'check_paths': 'true',
-            'base_path': "content"
+        "pymdownx.progressbar": {},
+        "pymdownx.blocks.tab": {},
+        "pymdownx.snippets": {
+            "check_paths": "true",
+            "base_path": "content"
         },
-        'pymdownx.magiclink': {},
-        # 'pymdownx.saneheaders': {},
-        'markdown_extension_mermaid': {},
+        "pymdownx.magiclink": {},
+        # "pymdownx.saneheaders": {},
+        "markdown_extension_mermaid": {},
     },
-    'output_format': 'html5'
+    "output_format": "html5"
 }
 
 # RSS and ATOM feeds
@@ -161,8 +169,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 # CACHING
-
-CACHE_CONTENT = True
-LOAD_CONTENT_CACHE = True
+CACHE_CONTENT = False
+LOAD_CONTENT_CACHE = False
 GZIP_CACHE = False
 DELETE_OUTPUT_DIRECTORY = False
