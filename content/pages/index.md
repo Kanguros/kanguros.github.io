4f3c9f56412eacbd872b1685cc63cdb4117b96ce
Kangur <urbanek.kamil@gmail.com>
2025-01-20 14:02:12 +0100

    Fixes

--- content/pages/index.md
+++ content/pages/index.md
@@ -3,9 +3,10 @@ Url:
 Save_as: index.html
 Template: main_page
 
-# Hi! I'm Kamil
+# Hi! I'm Kamil!
 
 Welcome to my digital notepad.
 Space where I share my notes and thoughts
 on programming, pipelines, automation,
 and an array of other, yet to be discovered, fancy stuff.
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -164,6 +164,13 @@ MARKDOWN = {
     "output_format": "html5"
 }
 
+SVG_HOME_K = """
+<svg width="40" height="40" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle; margin-right: 8px;">
+    <!-- Outer border -->
+    <rect x="5" y="5" width="90" height="90" rx="15" fill="var(--main-font-color)" stroke="#404040" stroke-width="4"></rect>
+    <text x="50%" y="50%" font-family="Arial, sans-serif" font-size="70" fill="#fff" text-anchor="middle" alignment-baseline="central">K</text>
+  </svg>
+"""
 SVG_POST = """<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-post" viewBox="0 0 16 16">
   <path d="M4 3.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5z"/>
   <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1"/>
